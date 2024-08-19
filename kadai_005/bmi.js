c124d1fed911b3bf762be5dff584d369763ebbbb
mugicha-design <167395735+mugicha-design@users.noreply.github.com>
2024-08-19 10:19:25 +0900

    bmi.jsを再アップロードしました。

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,7 +4,7 @@ let bodyWeight = 68;
 //身長：1.7m
 let bodyHeight = 1.7;
 
-let totalHeight = (bodyHeight*bodyHeight);
-
 //計算式：[体重(kg)]÷[身長(m)×身長(m)]
-console.log (bodyWeight / totalHeight);
+let bmi = bodyWeight /(bodyHeight * bodyHeight);
+
+console.log(bmi);
