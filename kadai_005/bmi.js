00f5933c93b1b387b2773e9fb227b4430d3c1fc3
mugicha-design <167395735+mugicha-design@users.noreply.github.com>
2024-08-19 09:49:16 +0900

    kadai_005をアップロードしました

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+//体重：68kg
+let bodyWeight = 68;
+
+//身長：1.7m
+let bodyHeight = 1.7;
+
+let totalHeight = (bodyHeight*bodyHeight);
+
+//計算式：[体重(kg)]÷[身長(m)×身長(m)]
+console.log (bodyWeight / totalHeight);
