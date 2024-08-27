dc6c13fe4fd31224918d7c4e91d1ca4739e14430
mugicha-design <167395735+mugicha-design@users.noreply.github.com>
2024-08-27 13:09:40 +0900

    kadai_020をアップロードしました

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const pushBtn = document.getElementById('btn');
+const pushText = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+    text.textContent = 'ボタンをクリックしました';
+    console.log(btn);
+});
